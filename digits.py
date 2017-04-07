4d5b89c68af7ec403bf1748ba190b6f3477d83e0
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-04-07 12:50:34 -0700

    initial commit

---
+++ digits.py
@@ -0,0 +1,15 @@
+
+def gen_foursets(iterable):
+    pass
+
+def fourset_permutations(fourset):
+    pass
+
+def random_product(fourlist):
+    pass
+
+def clever_product(fourlist, total):
+    pass
+
+def optimal_product(fourlist):
+    pass
